3200d07c56c407ab206c33031a72a3a983911565
Kaloyan Nachev <kaloyan.r.nachev@gmail.com>
2016-03-06 21:32:56 +0200

    screenhosts and ui

--- bootloader/boot2/start.cpp
+++ bootloader/boot2/start.cpp
@@ -15,27 +15,11 @@ static const uint kernelFile =	0x01'00'00'00;	//16 MB
 
 int start()
 {
-    
-    // MemoryService memoryService;
     DiskService diskService;
     VgaService vgaService;
 
     vgaService.ClearScreen(0x00);
 
-    //  vgaService.SetCursorColor(0x07);
-    //  vgaService.SetCursorPos(16, 3);
-
-    /* vgaService.Print("+-----------------------------------------+");
-    vgaService.SetCursorPos(16, 4);
-    vgaService.Print("|    SOS 32 Bit C++ Kernel Executing!     |");
-    vgaService.SetCursorPos(16, 5);
-    vgaService.Print("+-----------------------------------------+");
-
-    vgaService.SetCursorPos(15, 10);
-    vgaService.SetCursorColor(0x0F);
-    vgaService.Print("I am preparing to load... Hold on, please... :)");
-
-	vgaService.SetCursorPos(0, 12);*/
 
     diskService.DetectPrimaryDisk();
     diskService.DisableInterrups();
@@ -78,51 +62,5 @@ int start()
 	}
 	
 	return 1;
-    //vgaService.Print(static_cast<uint>(diskService.GetBootRecord().reservedSectorsCount));
-
-    //byte* buffer = static_cast<byte*>(memoryService.Allocate(sizeof(byte)* DiskService::SECTOR_SIZE_BYTES));
-
-    //diskService.ReadFromHDD(8216,1 ,buffer);
-
-    //vgaService.Print("\nRead complete!");
-
-    //buffer[11] = '\0';
-
-    //vgaService.SetCursorPos(0, 12);
-    //vgaService.Print(reinterpret_cast<char*>(buffer));
-
-    /* byte a = 0x00;
-    byte b = 0x10;
-    while(true)
-	{
-	    uint x = 0;
-	    vgaService.SetCursorColor(b);
-	    byte c = a;
-	    a = b;
-	    b = c;
-	    for(uint i = 0; i < VgaService::COLUMNS; ++i)
-		{
-		    for(uint j = 0; j < 40000; ++j)
-			{
-			    vgaService.SetCursorPos(x, 22);
-			}
-		    vgaService.Print(' ');
-		    ++x;
-		}
-	}*/
+   
 }
-
-/*void PrintDiskInfo(DiskService& diskService, VgaService& vgaService)
-{
-    const FAT32BootRecord& bootRecord = diskService.GetBootRecord();
-    const FAT32ExtendedBootRecord& extendedRecord = diskService.GetExtendedBootRecord();
-
-    vgaService.SetCursorPos(0, 10);
-    vgaService.SetCursorColor(0x0B);
-    vgaService.Print("OEM: ");
-    vgaService.Print(bootRecord.oem);
-    vgaService.Print(" Label: ");
-    vgaService.Print(extendedRecord.volumeLabel, 11);
-    vgaService.Print(" Identifier: ");
-    vgaService.Print(extendedRecord.identifier, 8);
-}*/
--- kernel/kernel.cpp
+++ kernel/kernel.cpp
@@ -24,6 +24,7 @@ static const uint KERNEL_ADDRESS = 0x10'00'00;                                  
 static const uint MEMORY_MANAGER_ADDRESS = CPUSystem::IDT_ADDRESS + CPUSystem::IDT_SIZE; //16.002 Mb
 
 void Interrupt();
+void DrawGUI(VGATextSystem&);
 
 VGATextSystem* v;
 
@@ -41,9 +42,12 @@ void kmain()
     v = vgaTextSystem;
 
     vgaTextSystem->ClearScreen();
-    vgaTextSystem->PrintText("Welcome to the real kernel!\n");
+
+    //vgaTextSystem->PrintText("Welcome to the real kernel!\n");
 
     cpuSystem->SetInterruptGate(0x00, (void*)&Interrupt);
+
+    DrawGUI(*vgaTextSystem);
 
     asm("hlt");
 
@@ -59,4 +63,48 @@ void Interrupt()
     v->ClearScreen();
     v->PrintText("OPS! Just BSODed kinda...");
     asm("hlt");
+}
+
+void DrawGUI(VGATextSystem& vgaSystem)
+{
+    Cursor& cursor = vgaSystem.GetCursor();
+
+    cursor.foregroundColor = 0x07;
+    cursor.x = 16;
+    cursor.y = 3;
+
+    vgaSystem.PrintText("+-----------------------------------------+");
+    cursor.x = 16;
+    cursor.y = 4;
+    vgaSystem.PrintText("|    SOS 32 Bit C++ Kernel Executing!     |");
+    cursor.x = 16;
+    cursor.y = 5;
+    vgaSystem.PrintText("+-----------------------------------------+");
+
+    cursor.x = 15;
+    cursor.y = 10;
+    cursor.foregroundColor = 0x0F;
+    vgaSystem.PrintText("I am preparing to load... Hold on, please... :)");
+
+    byte a = 0x00;
+    byte b = 0x10;
+
+    while(true)
+	{
+	    uint x = 0;
+	    cursor.foregroundColor = b;
+	    byte c = a;
+	    a = b;
+	    b = c;
+	    for(uint i = 0; i < vgaSystem.GetDriver()->GetParameters().textColumns; ++i)
+		{
+		    for(uint j = 0; j < 100000; ++j)
+			{
+			    cursor.y = 22;
+			    cursor.x = x;
+			}
+		    vgaSystem.PrintText(" ");
+		    ++x;
+		}
+	}
 }
